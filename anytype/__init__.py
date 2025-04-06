5d90b5d9b4170b21b457b83120f5a295a756a2e5
Charles K. Neimog <charlesneimog@outlook.com>
2025-04-06 10:46:36 -0300

    refactoring api calls

--- anytype/__init__.py
+++ anytype/__init__.py
@@ -3,6 +3,8 @@ from .space import Space
 from .object import Object
 from .block import Block
 from .type import Type
-from .template import Template 
+from .template import Template
 from .relation import Relation
 from .config import API_CONFIG
+from .config import END_POINTS as _END_POINTS
+from .api import apiEndpoints
